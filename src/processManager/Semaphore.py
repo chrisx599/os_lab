8e1accfbf5fce822186f88af07a625fb4ff91f6b
chr1ce <60588286+chrisx599@users.noreply.github.com>
2023-05-12 18:48:14 +0800

    update Semaphore.py

--- src/processManager/Semaphore.py
+++ src/processManager/Semaphore.py
@@ -1,5 +1,6 @@
 import queue
 from lib.logger import logger
+from Process import Process
 
 class Semaphore():
     def __init__(self, initial:int) -> bool:
@@ -19,13 +20,17 @@ class Semaphore():
     def wait(self):
         self.value -= 1
         if self.value < 0:
+            # acquire this process
+            process = Process.get_running_pcb()
             # add this process to queue
+            self.queue.put(process)
             # block()
             pass
     
     def signal(self):
         self.value += 1
         if self.value <= 0:
+            process = Process.get_running_pcb()
             # remove a process P from queue
             # wakeup(P)
             pass
