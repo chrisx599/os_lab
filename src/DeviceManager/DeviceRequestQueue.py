c9ef007e3a5cd12b10a9ff0d663ae605d54dce85
Tsing-Klay <85896975+Tsing-Klay@users.noreply.github.com>
2023-05-16 15:22:12 +0800

    update DeviceManager

---
+++ src/DeviceManager/DeviceRequestQueue.py
@@ -0,0 +1,16 @@
+﻿# 设备请求队列类
+class DeviceRequestQueue:
+    def __init__(self):
+        self.queue = []  # 设备请求队列，用列表实现
+
+    # 添加设备请求
+    def add_request(self, pid, dev_type, dev_num):
+        self.queue.append((pid, dev_type, dev_num))
+
+    # 获取设备请求
+    def get_request(self):
+        if len(self.queue) > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
