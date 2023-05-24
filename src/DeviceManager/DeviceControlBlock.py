676342524733c8c972213695ff7b688b7d7fc9d4
qingyao <lkx1447358276@163.com>
2023-05-24 19:25:57 +0800

    2023-5-24 19：25与设备对接完成,测试完成

--- src/DeviceManager/DeviceControlBlock.py
+++ src/DeviceManager/DeviceControlBlock.py
@@ -1,16 +1,16 @@
 ﻿from utils.logger import logger
-
+from queue import Queue
 # 设备控制块类
 class DeviceControlBlock:
     def __init__(self, dev_type, dev_id):
         self.dev_type = dev_type  # 设备类型
         self.dev_id = dev_id  # 设备编号
         self.status = "idle"  # 设备状态
-        self.queue = []  # 设备队列，存储等待该设备资源的进程信息
+        self.queue = Queue()  # 设备队列，存储等待该设备资源的进程信息
         logger.info('Successfully initialized device:'+str(dev_id)+' '+dev_type+'.')
 
     def get_dcb_queue(self):
-        if len(self.queue) > 0:
-            return self.queue.pop(0)
+        if not self.queue.empty():
+            return self.queue.get()
         else:
             return None
