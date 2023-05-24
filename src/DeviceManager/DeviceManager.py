99133c73a6666bcd4231b5f3edd07f5a0a015637
Tsing-Klay <85896975+Tsing-Klay@users.noreply.github.com>
2023-05-24 16:39:14 +0800

    update DeviceManager

--- src/DeviceManager/DeviceManager.py
+++ src/DeviceManager/DeviceManager.py
@@ -1,43 +1,55 @@
-﻿from DeviceControlBlock import *
+﻿import sys
+import threading
+sys.path.append('..')
+from DeviceControlBlock import *
 from DeviceStatusTable import *
 from DeviceRequestQueue import *
 import time
+from utils.logger import logger
 from utils.Container import *
+from processManager import PCB
+
+def run(dcb):
+    #参数interrupt_event, interrupt_pcb_queue
+    while True:
+        if dcb.status == "idle":
+            dcb_request = dcb.get_dcb_queue()
+            pcb,dev_type,dev_num = dcb_request
+            if pcb != None:
+                execute_operation(pcb, dev_type, dev_num)
+                time.sleep(3)
+                pcb.set_event = 1
+                interrupt_pcb_queue.put(pcb)
+                interrupt_event.set()
+
+#t=threading.Thread(target=run, name=name)
 
 #处理请求，调用设备
-def use_dev():
+def use_dev(drq,dst):
     # 获取设备请求
     request = drq.get_request()
-    print(request)
     if request is not None:
-        pid, dev_type, dev_num = request
+        pcb, dev_type, dev_num = request
         dcb = dst.get_dev(dev_num)
-        if dcb is not None and dcb.status == "idle":
-            dcb.status = "busy"  # 更新设备状态为忙碌
-            # 执行设备操作
-            execute_operation(pid, dev_type, dev_num)
-        else:
+        if dcb is not None:
             dcb.queue.append(request)  # 将进程加入设备队列
     else:
         time.sleep(1)  # 暂停一段时间，等待下一次设备请求
 
 #执行设备操作
-@inject("interrupt_event","interrupt_type_queue","interrupt_ack_event")
-def execute_operation(pid,dev_type,dev_num,interrupt_event,interrupt_type_queue,interrupt_ack_event):
-    time.sleep(5)
-    interrupt_event.set()
-    if dev_type == "printer":
-        interrupt_ack_event.wait()
-        print("***模拟操作系统打印机***")
-        interrupt_type_queue.put(4)
-        print(interrupt_type_queue.get())
-    elif dev_type == "keyboard":
+def execute_operation(pcb,dev_type,dev_num):
+    print("Device" + dev_type + "is using by " + pcb.get_PID())
+    if dev_num == 1:
         input_tmp = input("请在键盘上输入\n")
-        interrupt_type_queue.put(3)
-        interrupt_type_queue.put(input_tmp)
+        # 确认input长度，写入内存
+        pcb.set_buffer_size()
+        pcb.set_buffer_adderss()
+    elif dev_num == 2:
+        print("***模拟操作系统打印机***")
+        print(pcb.get_buffer_content())
+    else:
+        pass
 
-    interrupt_event.clear()
-    interrupt_ack_event.clear()
 
 #释放设备
 def release_dev(dev_type,dev_num):
@@ -65,19 +77,22 @@ if __name__ == "__main__":
     drq = DeviceRequestQueue()
 
     # 添加设备到设备状态表
-    dst.add_dev("disk", 1)
+    dst.add_dev("keyboard",1)
     dst.add_dev("printer", 2)
+    t = threading.Thread(target=run,args=[dst.get_dev(1)],name="keyboard")
+    t.start()
 
     #查看所有设备
     dst.print_all_devs()
 
     #删除指定设备
-    dst.del_dev("dsik",1)
+    dst.del_dev("disk",1)
 
     dst.print_all_devs()
 
     #添加设备申请
-    drq.add_request(1,"printer",2)
+    pcb = PCB
+    drq.add_request(pcb,"printer",2)
 
     # 处理设备请求
     while True:
