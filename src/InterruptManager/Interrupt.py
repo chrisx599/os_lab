d8757fa7dd2a46055997731e2f1d6f7c8d7af13d
xunqingyao <lkx1447358276@163.com>
2023-05-25 18:05:26 +0800

    2023-5-25 18：07 线程关闭接口

--- src/InterruptManager/Interrupt.py
+++ src/InterruptManager/Interrupt.py
@@ -12,9 +12,9 @@ import ctypes
 class Interrput(threading.Thread):
 
     @inject("ready_pcb_queue", "interrupt_pcb_queue", "interrupt_event",
-            "interrupt_message_queue", "process_over_event", "memory", "block_pcb_queue")
+            "interrupt_message_queue", "process_over_event", "memory", "block_pcb_queue", "exit_event")
     def __init__(self, ready_pcb_queue, interrupt_pcb_queue, interrupt_event,
-                 interrupt_message_queue, process_over_event, memory, block_pcb_queue):
+                 interrupt_message_queue, process_over_event, memory, block_pcb_queue, exit_event):
         super().__init__()
         self.ready_pcb_queue = ready_pcb_queue
         self.interrupt_event = interrupt_event
@@ -23,52 +23,54 @@ class Interrput(threading.Thread):
         self.process_over_event = process_over_event
         self.memory = memory
         self.block_pcb_queue = block_pcb_queue
+        self.exit_event = exit_event
 
     def run(self):
-        try:
-            while True:
-                if not self.interrupt_event.is_set():
-                    self.interrupt_event.wait()
-                    self.interrupt_pcb = self.interrupt_pcb_queue.get()
-                    type = 0
-                    if self.interrupt_pcb != None:
-                        type = self.interrupt_pcb.get_event()
-                    if type == 1:# ok
-                        self.interrupt_pcb.set_state(PCB.PROCESS_READY)
-                        self.ready_pcb_queue.put(self.interrupt_pcb)
-                        self.interrupt_event.clear()
-                    elif type == 2:
-                        do_IRQ(self.interrupt_pcb.get_device_id())
-                        self.interrupt_pcb.set_state(PCB.PROCESS_BLOCK)
-                        self.block_pcb_queue.put(self.interrupt_pcb)
-                        self.process_over_event.set()
-                        self.interrupt_event.clear()
-                    else:
-                        page_num = self.interrupt_message_queue.get()
-                        program_num = self.interrupt_message_queue.get()
-                        out_page = self.interrupt_message_queue.get()
-                        self.memory.program_deal_page_fault(page_num, program_num, out_page)
-                        self.interrupt_event.clear()
-        finally:
-            print("interrupt thread end")
+        while True:
+            if not self.interrupt_event.is_set():
+                self.interrupt_event.wait()
+                if self.exit_event.is_set():
+                    print("interrupt thread end")
+                    return
+                self.interrupt_pcb = self.interrupt_pcb_queue.get()
+                type = 0
+                if self.interrupt_pcb != None:
+                    type = self.interrupt_pcb.get_event()
+                if type == 1:# ok
+                    self.interrupt_pcb.set_state(PCB.PROCESS_READY)
+                    self.ready_pcb_queue.put(self.interrupt_pcb)
+                    self.interrupt_event.clear()
+                elif type == 2:
+                    do_IRQ(self.interrupt_pcb.get_device_id())
+                    self.interrupt_pcb.set_state(PCB.PROCESS_BLOCK)
+                    self.block_pcb_queue.put(self.interrupt_pcb)
+                    self.process_over_event.set()
+                    self.interrupt_event.clear()
+                else:
+                    page_num = self.interrupt_message_queue.get()
+                    program_num = self.interrupt_message_queue.get()
+                    out_page = self.interrupt_message_queue.get()
+                    self.memory.program_deal_page_fault(page_num, program_num, out_page)
+                    self.interrupt_event.clear()
 
-    def get_id(self):
 
-        # returns id of the respective thread
-        if hasattr(self, '_thread_id'):
-            return self._thread_id
-        for id, thread in threading._active.items():
-            if thread is self:
-                return id
-
-    def stop(self):
-        thread_id = self.get_id()
-        self.interrupt_event.set()
-        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
-                                                         ctypes.py_object(SystemExit))
-        if res > 1:
-            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
-            print('Exception raise failure')
+    # def get_id(self):
+    #
+    #     # returns id of the respective thread
+    #     if hasattr(self, '_thread_id'):
+    #         return self._thread_id
+    #     for id, thread in threading._active.items():
+    #         if thread is self:
+    #             return id
+    #
+    # def stop(self):
+    #     thread_id = self.get_id()
+    #     self.interrupt_event.set()
+    #     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
+    #                                                      ctypes.py_object(SystemExit))
+    #     if res > 1:
+    #         ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+    #         print('Exception raise failure')
 
 @inject("device_request_queue", "device_status_table")
 def do_IRQ(pcb, device: int, device_request_queue, device_status_table):
--- src/TimeManager/Timer.py
+++ src/TimeManager/Timer.py
@@ -11,54 +11,56 @@ class Timer(threading.Thread):
     system_time = 0
     timeout_event = None
 
-    @inject("os_timer_messager", "timeout_event", "running_event", "process_over_event", "interrupt_event")
-    def __init__(self, os_timer_messager, timeout_event, running_event, process_over_event, interrupt_event):
+    @inject("os_timer_messager", "timeout_event", "running_event", "process_over_event", "interrupt_event", "exit_event")
+    def __init__(self, os_timer_messager, timeout_event, running_event, process_over_event, interrupt_event, exit_event):
         threading.Thread.__init__(self)
         self.os_timer_messager = os_timer_messager
         self.timeout_event = timeout_event
         self.running_event = running_event
         self.process_over_event = process_over_event
         self.interrupt_event = interrupt_event
+        self.exit_event = exit_event
 
     def run(self) -> None:
         print("Timer thread start")
-        try:
-            while True:
-                if (not self.running_event.is_set()) or self.timeout_event.is_set():
-                    if not self.running_event.is_set():
-                        print("waiting running_event set")
-                        self.running_event.wait()
-                    else:
-                        continue
-                print("time_slice start")
-                time_slice = self.os_timer_messager.get()
-                i = 0
-                while i < time_slice * 5 and not self.process_over_event.is_set():
-                    if not self.interrupt_event.is_set():
-                        i += 1
-                        self.process_over_event.wait(0.001)
-                self.os_timer_messager.put(i)
-                self.timeout_event.set()
-        finally:
-            print("Timer thread end")
+        while True:
+            if (not self.running_event.is_set()) or self.timeout_event.is_set():
+                if not self.running_event.is_set():
+                    print("waiting running_event set")
+                    self.running_event.wait()
+                    if self.exit_event.is_set():
+                        print("Timer thread end")
+                        return
+                else:
+                    continue
+            print("time_slice start")
+            time_slice = self.os_timer_messager.get()
+            i = 0
+            while i < time_slice * 5 and not self.process_over_event.is_set():
+                if not self.interrupt_event.is_set():
+                    i += 1
+                    self.process_over_event.wait(0.001)
+            self.os_timer_messager.put(i)
+            self.timeout_event.set()
 
-    def get_id(self):
 
-        # returns id of the respective thread
-        if hasattr(self, '_thread_id'):
-            return self._thread_id
-        for id, thread in threading._active.items():
-            if thread is self:
-                return id
-
-    def stop(self):
-        thread_id = self.get_id()
-        self.running_event.set()
-        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
-                                                         ctypes.py_object(SystemExit))
-        if res > 1:
-            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
-            print('Exception raise failure')
+    # def get_id(self):
+    #
+    #     # returns id of the respective thread
+    #     if hasattr(self, '_thread_id'):
+    #         return self._thread_id
+    #     for id, thread in threading._active.items():
+    #         if thread is self:
+    #             return id
+    #
+    # def stop(self):
+    #     thread_id = self.get_id()
+    #     self.running_event.set()
+    #     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
+    #                                                      ctypes.py_object(SystemExit))
+    #     if res > 1:
+    #         ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+    #         print('Exception raise failure')
 
 if __name__ == "__main__":
     run_code = -1
