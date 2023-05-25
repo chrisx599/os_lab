ac135c3e56c3407c2b3b16d5952a4bc7785fe7b8
chr1ce <60588286+chrisx599@users.noreply.github.com>
2023-05-25 16:36:49 +0800

    update memoryui

--- src/UIManager/MemoryUI.py
+++ src/UIManager/MemoryUI.py
@@ -9,13 +9,15 @@ from PyQt6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PyQt6.QtWidgets import (QApplication, QHeaderView, QSizePolicy, QTableWidget,
-    QTableWidgetItem, QWidget)
+    QTableWidgetItem, QWidget, QLabel)
+
+from qfluentwidgets import ProgressBar
 
 class Ui_MemoryViewer(object):
     def setupUi(self, MemoryViewer):
         if not MemoryViewer.objectName():
             MemoryViewer.setObjectName(u"MemoryViewer")
-        MemoryViewer.resize(500, 500)
+        MemoryViewer.resize(500, 550)
         self.MemoryList = QTableWidget(MemoryViewer)
         if (self.MemoryList.columnCount() < 16):
             self.MemoryList.setColumnCount(16)
@@ -25,6 +27,25 @@ class Ui_MemoryViewer(object):
         self.MemoryList.setGeometry(QRect(0, 0, 500, 500))
         self.MemoryList.setRowCount(16)
         self.MemoryList.setColumnCount(16)
+
+        ##############################################################
+        # 设置进度条来展示内存占用率
+        self.progressBar = ProgressBar(MemoryViewer)
+        self.progressBar.setValue(50)
+        self.progressBar.setGeometry(0, 500, 500, 50)
+        # 设置两个标签来显示已占用内存和总内存
+        self.already_used = QLabel(MemoryViewer)
+        self.already_used.setGeometry(50, 520, 150, 30)
+        self.already_used.setText("test")
+        self.all_mem = QLabel(MemoryViewer)
+        self.all_mem.setGeometry(250, 520, 150, 30)
+        self.all_mem.setText("dawdaw")
+        # 设置一个标签来显示已占用百分比
+        # self.rate = QLabel(MemoryViewer)
+        # self.rate.setGeometry(400, 530, 100, 20)
+        # self.rate.setText("dddd")
+        ##############################################################
+
         # 设置格子的大小
         cell_size = 28  # 设置每个格子的大小
         
@@ -52,6 +73,7 @@ class MemoryUI():
         self.ui = Ui_MemoryViewer()
         self.ui.setupUi(self.window)
 
+
     def set_color(self, memory_matrix):
         for i in range(16):
             for k in range(16):
@@ -61,6 +83,12 @@ class MemoryUI():
                     # item = QTableWidgetItem("Cell Text")
                     item.setBackground(color)
                     self.ui.MemoryList.setItem(i, k, item)
+
+    def set_label(self, already_used, all_mem):
+        rate = already_used / all_mem
+        self.ui.progressBar.setValue(int(rate))
+        self.ui.already_used.setText("已占用内存:" + str(already_used))
+        self.ui.all_mem.setText("总内存:" + str(all_mem))
 
 if __name__ == "__main__":
     app = QApplication([])
