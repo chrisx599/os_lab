d40747b7b10189e49791ac288da2215dd9827d67
xunqingyao <lkx1447358276@163.com>
2023-05-26 16:08:35 +0800

    need to get

---
+++ src/utils/test.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# @FileName : test.py.py
+# @Time     : 2023/5/25 3:48
+# @Author   : qingyao
+import sys
+# sys.path.append("D:\\pythonCode\\final\\os_lab\\src")
+if __name__ == "__main__":
+    print(sys.path)
